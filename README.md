e451fc7319dfe41239dafc86c8a95ed39cb050f6
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-12-18 22:24:52 +0530

    First commit on main

---
+++ README.md
@@ -0,0 +1,108 @@
+# Effort Logger V2 with Planning Poker
+
+## Overview
+
+Effort Logger V2 with Planning Poker is a project aimed at facilitating project estimation and planning using the Planning Poker technique. This tool allows teams to collaboratively estimate the effort required for tasks in a project, fostering communication and improving accuracy in time estimation. The Planning Poker technique, well-known in agile methodologies, involves team members providing individual estimates for specific tasks, followed by open discussions to converge on a consensus estimate. Effort Logger V2 enhances this technique by providing a dedicated platform for teams to engage in this estimation process seamlessly.
+
+## Features
+
+- **Planning Poker:** Leverage the Planning Poker technique to engage team members in estimating the effort required for tasks.
+  
+- **Effort Logging:** Record and track estimated efforts for each task, providing a historical view of estimations and actual efforts.
+
+- **User-Friendly Interface:** An intuitive and user-friendly interface ensures easy navigation for both experienced and new users.
+
+- **Collaborative Estimation:** Foster teamwork by allowing team members to participate in the estimation process, leading to more accurate and well-rounded estimates.
+
+## Installation
+
+To get started, follow these steps:
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your-username/EffortLogger-With-Planning-Poker.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd EffortLogger
+    ```
+
+3. Check Maven version, and if not installed - install a stable release, then go to Visual Studio and run:
+
+    ```bash
+    mvn -v
+    ```
+4. Make sure you put in your AWS Credentials (AWS_ACCESS_KEY & AWS_SECRET_KEY) inside AWSBackup.java:
+
+
+5. Start the application:
+
+    ```bash
+    'C:\Program Files\Java\jdk1.8.0_311\bin\java.exe' '-cp' 'C:\Users\$YOUR$NAME\AppData\Local\Temp\cp_f0zi1x4w4ps1m9f8j8hog6o1l.jar' 'com.cse360.Main'
+    ```
+    
+
+Sign Up as a new user, and proceed!:)
+
+## Usage
+
+1. **Create a Project:**
+    - Start by creating a new project and defining the tasks that need estimation.
+
+2. **Invite Team Members:**
+    - Invite team members to join the estimation session.
+
+3. **Planning Poker:**
+    - Initiate Planning Poker sessions for each task, allowing team members to provide their estimates.
+
+4. **Record Efforts:**
+    - Record the final estimates and track them over time.
+
+5. **Review and Analyze:**
+    - Use the historical data to analyze estimation accuracy and make informed decisions for future projects.
+
+## Contributing
+
+If you'd like to contribute to this project, follow these steps:
+
+1. Fork the repository.
+
+2. Create a new branch:
+
+    ```bash
+    git checkout -b feature/new-feature
+    ```
+
+3. Make your changes and commit them:
+
+    ```bash
+    git commit -m "Add new feature"
+    ```
+
+4. Push to the forked repository:
+
+    ```bash
+    git push origin feature/new-feature
+    ```
+
+5. Open a pull request.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Acknowledgments
+
+- Thanks to the Planning Poker technique for providing an effective way to estimate project efforts.
+
+Feel free to customize this README file according to your project's specific details and requirements.
+
+## Contributors:
+### Caiden McGregor
+### Chaitanya Chaurasia
+### Gavin Holtzapple
+### Harish Chaurasia
+### Neel Madhav Dogra
---
+++ src/main/java/com/cse360/AWSBackup.java
@@ -0,0 +1,109 @@
+package com.cse360;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+// this is where we import all the aws imports
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import java.io.File;
+
+public class AWSBackup extends Application {
+
+    private Label backupStatusLabel;
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Backup Center");
+
+        AnchorPane root = new AnchorPane();
+
+        // Creating the labels
+        Label titleLabel = new Label("Backup Center");
+        titleLabel.setFont(Font.font("Bold", 20.0));
+        AnchorPane.setTopAnchor(titleLabel, 20.0);
+        AnchorPane.setLeftAnchor(titleLabel, 180.0);
+
+        Label instructionLabel = new Label("Perform Data Backup");
+        instructionLabel.setFont(Font.font("System Bold", 13.0));
+        AnchorPane.setTopAnchor(instructionLabel, 115.0);
+        AnchorPane.setLeftAnchor(instructionLabel, 185.0);
+
+        // label to show backup status
+        backupStatusLabel = new Label();
+        backupStatusLabel.setFont(Font.font("System Bold", 13.0));
+        AnchorPane.setTopAnchor(backupStatusLabel, 220.0);
+        AnchorPane.setLeftAnchor(backupStatusLabel, 170.0);
+        backupStatusLabel.setVisible(false); // Initially hide this label
+
+        // Create backup button
+        Button backupButton = new Button("Start Backup");
+        backupButton.setOnAction(event -> performBackup());
+        AnchorPane.setTopAnchor(backupButton, 140.0);
+        AnchorPane.setLeftAnchor(backupButton, 200.0);
+
+        // Adding components to the AnchorPane
+        root.getChildren().addAll(titleLabel, instructionLabel, backupButton, backupStatusLabel);
+
+        Scene scene = new Scene(root, 500, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private void performBackup() {
+        System.out.println("Backup in progress...");
+        // System.out.println("Backup complete.");
+
+        uploadFilesToS3Bucket();
+
+        // Show "Backup created" message
+        backupStatusLabel.setText("Backup Created Successfully...");
+        backupStatusLabel.setVisible(true);
+
+        System.out.println("Backup complete.");
+    }
+
+    // Example method for AWS S3 backup logic
+    private void uploadFilesToS3Bucket() {
+        // Hardcoded AWS credentials (not recommended for production)
+        BasicAWSCredentials awsCreds = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY);
+
+        // Creating an S3 client
+        AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                .withRegion(Regions.YOUR_REGION) // Choose the appropriate region
+                .build();
+
+        // Defining the bucket name and the file to upload
+        String bucketName = BUCKET_NAME;
+        String fileObjKeyName = OBJECT_NAME;
+        String fileName = FILE_PATH;
+
+        try {
+            // Uploading the file to S3
+            s3Client.putObject(new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName)));
+            System.out.println("Data backup created successfully.");
+        } catch (Exception e) {
+            // Handle exceptions
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void openWindow() {
+        Stage stage = new Stage();
+        start(stage);
+    }
+}
